f5eb611c45bf77decdb7059ed6f868b1698d0556
Nitsan <mintaz>
2018-01-04 18:00:23 +0200

    add debug to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const config = {
-    entry: './src/app/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template: './src/index.html'})
-    ],
-    devServer: {
-        proxy: {
-            "*": "http://localhost:3000"
-        }
+  entry: './src/app/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ],
+  devtool: 'cheap-module-inline-source-map',
+  devServer: {
+    proxy: {
+      "*": "http://localhost:3000"
     }
+  }
 };
 
 module.exports = config;
